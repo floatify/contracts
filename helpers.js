4345d9e45b044f8589e4ed149a4d2d40f50c92f0
mds <17163988+mds1@users.noreply.github.com>
2020-03-14 08:05:28 -0700

    Add USDC contract info

--- helpers.js
+++ helpers.js
@@ -7,6 +7,7 @@ const daiAbi = require('./externalAbis/DAI.json').abi;
 const cdaiAbi = require('./externalAbis/cDAI.json').abi;
 const chaiAbi = require('./externalAbis/chai.json').abi;
 const makerAbi = require('./externalAbis/maker.json').abi;
+const usdcAbi = require('./externalAbis/usdc.json').abi;
 const saiAbi = require('./externalAbis/sai.json').abi;
 const potAbi = require('./externalAbis/pot.json').abi;
 
@@ -14,6 +15,7 @@ const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 const cdaiAddress = '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643';
 const chaiAddress = '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215';
 const makerAddress = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2';
+const usdcAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
 const kyberAddress = '0x818E6FECD516Ecc3849DAf6845e3EC868087B755'; // Kyber Network Proxy address
 const saiAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359';
 const potAddress = '0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7';
@@ -24,6 +26,7 @@ const DaiContract = new web3.eth.Contract(daiAbi, daiAddress);
 const CdaiContract = new web3.eth.Contract(cdaiAbi, cdaiAddress);
 const ChaiContract = new web3.eth.Contract(chaiAbi, chaiAddress);
 const MakerContract = new web3.eth.Contract(makerAbi, makerAddress);
+const UsdcContract = new web3.eth.Contract(usdcAbi, usdcAddress);
 const SaiContract = new web3.eth.Contract(saiAbi, saiAddress);
 const PotContract = new web3.eth.Contract(potAbi, potAddress);
 
@@ -123,12 +126,14 @@ module.exports = {
   CdaiContract,
   ChaiContract,
   SaiContract,
+  UsdcContract,
   PotContract,
   // Addresses
   daiAddress,
   cdaiAddress,
   chaiAddress,
   makerAddress,
+  usdcAddress,
   kyberAddress,
   potAddress,
   saiAddress,
